1dbe01b03612d38cea6cd03cc700fc59ee4d6dd5
Elminsterxld <73755282+Elminsterxld@users.noreply.github.com>
2024-07-01 00:45:17 +0300

    Create log.js

---
+++ log.js
@@ -0,0 +1,45 @@
+class Log {
+    static getCurrentTime() {
+        const date = new Date();
+        return date.toLocaleTimeString();
+    }
+
+    static writeToFile(logMessage) {
+        fs.appendFileSync('./output/log.txt', logMessage + '\n', 'utf8');
+    }
+
+    static success(message) {
+        const currentTime = this.getCurrentTime();
+        const logMessage = `[${currentTime}] [SUCCESS] ${message}`;
+        console.log(chalk.magenta(`[${currentTime}]`) + chalk.green(' [SUCCESS] ') + chalk.white(message));
+        this.writeToFile(logMessage);
+    }
+
+    static error(message) {
+        const currentTime = this.getCurrentTime();
+        const logMessage = `[${currentTime}] [ERROR] ${message}`;
+        console.log(chalk.magenta(`[${currentTime}]`) + chalk.red(' [ERROR] ') + chalk.white(message));
+        this.writeToFile(logMessage);
+    }
+
+    static info(message) {
+        const currentTime = this.getCurrentTime();
+        const logMessage = `[${currentTime}] [INFO] ${message}`;
+        console.log(chalk.magenta(`[${currentTime}]`) + chalk.blue(' [INFO] ') + chalk.white(message));
+        this.writeToFile(logMessage);
+    }
+
+    static warning(message) {
+        const currentTime = this.getCurrentTime();
+        const logMessage = `[${currentTime}] [WARNING] ${message}`;
+        console.log(chalk.magenta(`[${currentTime}]`) + chalk.yellow(' [WARNING] ') + chalk.white(message));
+        this.writeToFile(logMessage);
+    }
+
+    static debug(message) {
+        const currentTime = this.getCurrentTime();
+        const logMessage = `[${currentTime}] [DEBUG] ${message}`;
+        console.log(chalk.magenta(`[${currentTime}]`) + chalk.gray(' [DEBUG] ') + chalk.white(message));
+        this.writeToFile(logMessage);
+    }
+}
